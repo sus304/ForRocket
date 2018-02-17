bug fix:simulator mass calculation process, add BarrownmanFlow caluclation fin mass

BarrowmanFlow/BarrowmanFlow.py
@@ -144,6 +144,13 @@ class Fin:
         shear = young / (2.0 * (1.0 + poisson)) # shear modulus
         self.Vf = Std_Atmo(altitude)[3] * np.sqrt(shear * 10.0 ** 9 / ((1.337 * AR ** 3 * Std_Atmo(altitude)[1] * (ramda + 1.0)) / (2.0 * (AR + 2.0) * (thickness / self.Cr) ** 3)))
         return self.Vf
+    
+    def center_of_gravity_for_fin(self, mass_body, cg_body, thickness, rho_fin):
+        AR = (self.Cr + self.Ct) * self.span * 0.5
+        self.mass_fin = AR * thickness * rho_fin * 4
+        cg_fin = self.distance + (self.Cr - (self.Ct ** 2 + self.Cr * self.Ct + self.Cr ** 2) / (3.0 * (self.Cr + self.Ct)))
+        cg_move = (cg_body * mass_body + cg_fin * self.mass_fin) / (mass_body + self.mass_fin)
+        return cg_move  # [m]
 
 
 class Stage:
@@ -278,7 +285,7 @@ class FinOptimize:
 
     def solve(self, config):
         cons = ({'type':'eq', 'fun': lambda param:self.equality(param, config)}, {'type':'ineq', 'fun': lambda param:self.inequality(param, config)})
-        result = minimize(self.cost, self.param_init, args=(config,), constraints=cons, method='SLSQP', options={'maxiter': 100})#, 'disp': True, })
+        result = minimize(self.cost, self.param_init, args=(config,), constraints=cons, method='SLSQP', options={'maxiter': 300})#, 'disp': True, })
         return result
 
 

BarrowmanFlow/fin_optimize.py
@@ -5,34 +5,37 @@ import BarrowmanFlow as bmf
 class Config:
     def __init__(self):
         ################## User Input ###################
-        self.length_body = 3.9663 # [m] from nose tip to body. without tail
-        self.diameter_body = 0.183 # [m]
-        self.length_cg = 2.541 # [m] from nose tip
+        self.length_body = 1.743 # [m] from nose tip to body. without tail
+        self.diameter_body = 0.154 # [m]
+        self.mass_body = 13.238  # [kg]
+        self.length_cg = 1.07216 # [m] from nose tip
 
-        self.shape_nose = 'ogive' # 'ogive' or 'double' or 'parabolic' or 'ellipse'
-        self.length_nose = 0.640 # [m]
+        self.shape_nose = 'double' # 'ogive' or 'double' or 'parabolic' or 'ellipse'
+        self.length_nose = 0.2412 # [m]
 
         self.diameter_tail = 0.0 # [m]
         self.length_tail = 0.0 # [m]
 
         self.offset_fin = 0.0 / 1000.0 # [m] from body end to fin end
-        self.root_chord = 300.0 / 1000.0 # [m]
+        self.root_chord = 190.0 / 1000.0 # [m]
 
-        self.tip_chord_max = 300.0 / 1000.0 # [m]
-        self.tip_chord_min = 10.0 / 1000.0 # [m]
-        self.leading_edge_chord_max = 300.0 / 1000.0 # [m]
+        self.tip_chord_max = 500.0 / 1000.0 # [m]
+        self.tip_chord_min = 50.0 / 1000.0 # [m]
+        self.leading_edge_chord_max = 500.0 / 1000.0 # [m]
         self.leading_edge_chord_min = 10.0 / 1000.0 # [m]
-        self.span_max = self.diameter_body # [m]
+        # self.span_max = self.diameter_body # [m]
+        self.span_max = 130.0e-3 # [m]
         self.span_min = 10.0 / 1000.0 # [m]
         
-        self.thickness_fin = 3.0 / 1000.0 # [m]
+        self.thickness_fin = 2.0 / 1000.0 # [m]
         self.young_modulus = 69.0 # [GPa]
         self.poisson_ratio = 0.3 # [-]
-        self.altitude = 6000.0 # [m]
+        self.rho_fin = 1380        
+        self.altitude = 842.0 # [m]
 
         self.Fst_max = 20.0
-        self.Fst_min = 13.0
-        self.theta_max = 10.0 # [deg] 後端後退角
+        self.Fst_min = 12.0
+        self.theta_max = 8.0 # [deg] 後端後退角
         self.theta_min = -50.0 # [deg] 後端後退角
 
         self.stage = bmf.Stage(self.diameter_body, self.length_body, self.length_cg)
@@ -68,7 +71,9 @@ def inequality(param, config):
     components.append(fin)
     config.stage.integrate(components)
     length_cp = config.stage.Lcp
-    Fst = (length_cp - config.length_cg) / config.length_body * 100.0
+    length_cg = fin.center_of_gravity_for_fin(config.mass_body, config.length_cg, config.thickness_fin, config.rho_fin)
+    Fst = (length_cp - length_cg) / config.length_body * 100.0
+    # Fst = (length_cp - config.length_cg) / config.length_body * 100.0
     theta = np.arctan2(tip + leading - root, span)
     theta = np.rad2deg(theta)
     
@@ -100,7 +105,7 @@ def cost(param, config):
         
 config = Config()        
 opt = bmf.FinOptimize()
-root_init = 0.3
+root_init = 0.1
 tip_init = 0.1
 leading_init = 0.1
 span_init = 0.1
@@ -119,17 +124,29 @@ span = result.x[3]
 offset_fin = result.x[4]
 
 fin = bmf.Fin(config.stage, root_chord, tip_chord, leading_edge_chord, span, config.length_body - offset_fin - root_chord)
+length_cg = fin.center_of_gravity_for_fin(config.mass_body, config.length_cg, config.thickness_fin, config.rho_fin)
 # fin.flutter_speed(young_modulus, poisson_ratio, thickness_fin, altitude)
 # tail = bmf.TaperBody(diameter_body, diameter_tail, length_tail, length_body)
 config.stage.integrate([config.nose, fin])
 
 print('*=============Result==============*')
 print('Length of C.P.:', config.stage.Lcp, '[m]')
-print('Safety Ratio of Length:', (config.stage.Lcp - config.length_cg) / config.length_body * 100.0, '[%]')
+print('Length of C.G.:', config.length_cg, '[m]')
+print('Safety Ratio of Length:', (config.stage.Lcp - length_cg) / config.length_body * 100.0, '[%]')
 print('Coefficient of Normal Force:', config.stage.CNa, '[deg^-1]')
 print('Coefficient of Pitch Damping Moment:', config.stage.Cmq, '[-]')
 print('Coefficient of Roll Damping Moment:', config.stage.Clp, '[-]')
 # print('Flutter Velocity:', fin.Vf, '[m/s]')
+print('Mass of Fin:', fin.mass_fin, '[kg]')
+print('Mass of Body + Fin:', config.mass_body + fin.mass_fin, '[kg]')
+print('*=================================*')
+
+print('*=============Size==============*')
+print('Root Chord:', root_chord, '[m]')
+print('Tip Chord:', tip_chord, '[m]')
+print('Leading Edge Chord:', leading_edge_chord, '[m]')
+print('Span:', span, '[m]')
+print('Offset:', offset_fin, '[m]')
 print('*=================================*')
 
 config.stage.plot()

Simulator/rocket_dynamics.py
@@ -86,7 +86,7 @@ class Rocket:
 
         prop = json_engine.get('Propellant')
         self.m0_ox = prop.get('Oxidizer Mass [kg]')
-        self.mdot_ox = prop.get('Oxidizer Mass Flow Rate [kg/s]')
+        # self.mdot_ox = prop.get('Oxidizer Mass Flow Rate [kg/s]')
         self.m0_f = prop.get('Fuel Mass Before Burn [kg]')
         self.mf_after = prop.get('Fuel Mass After Burn [kg]')
         self.mdot_f = prop.get('Fuel Mass Flow Rate [kg/s]')
@@ -122,6 +122,25 @@ class Rocket:
         self.Ae = self.Ath * self.eps
         self.de = np.sqrt(self.Ae * 4.0 / np.pi)
 
+        # Mass interpolate
+        self.mdot_p_log = thrust_array / (self.Isp * 9.80665)
+        self.mdot_p = interpolate.interp1d(time_array, self.mdot_p_log, kind='linear', bounds_error=False, fill_value=(0.0, 0.0))
+        self.mdot_f = interpolate.interp1d(time_array, np.array([self.mdot_f]*len(time_array)), kind='linear', bounds_error=False, fill_value=(0.0, 0.0))
+        self.mdot_ox_log = self.mdot_p_log - self.mdot_f(time_array)
+        self.mdot_ox = interpolate.interp1d(time_array, self.mdot_ox_log, kind='linear', bounds_error=False, fill_value=(0.0, 0.0))        
+        
+        mf_log_ode = odeint(lambda x, t: -self.mdot_f(t), self.m0_f, time_array)
+        self.mf_log = mf_log_ode[0][0]
+        for mf in mf_log_ode[1:]:
+            self.mf_log = np.append(self.mf_log, mf[0])
+        self.mf = interpolate.interp1d(time_array, self.mf_log, kind='linear', bounds_error=False, fill_value=(self.mf_log[0], self.mf_log[-1]))        
+        
+        mox_log_ode = odeint(lambda x, t: -self.mdot_ox(t), self.m0_ox, time_array)
+        self.mox_log = mox_log_ode[0][0]
+        for mox in mox_log_ode[1:]:
+            self.mox_log = np.append(self.mox_log, mox[0])
+        self.mox = interpolate.interp1d(time_array, self.mox_log, kind='linear', bounds_error=False, fill_value=(self.mox_log[0], 0.0))        
+
         self.Wind_refAltitude = json.get('Environment').get('Wind Mesurement Height [m]')
         self.Wind_power_exp = json.get('Environment').get('Wind Power Law Exponential Coefficient')
 
@@ -149,8 +168,10 @@ def dynamics(x, t, rocket, solver):
     Vel_Body = x[6:9]
     omega_Body = x[9:12]
     quat = x[12:16]
-    mf = x[16]
-    mox = x[17]
+    # mf = x[16]
+    mf = rocket.mf(t)
+    # mox = x[17]
+    mox = rocket.mox(t)
     quat = coord.quat_normalize(quat)
     on_launcher = altitude / np.sin(np.deg2rad(np.abs(rocket.elevation0))) < rocket.launcher_rail and t < 2.0
 
@@ -192,22 +213,13 @@ def dynamics(x, t, rocket, solver):
         mdot_f = 0.0
         mdot_ox = 0.0
     else:
-        mdot_p = rocket.thrust(t) / (rocket.Isp * g0)
-        mdot_f = rocket.mdot_f
-        mdot_ox = mdot_p - mdot_f
+        mdot_p = rocket.mdot_p(t)
+        mdot_f = rocket.mdot_f(t)
+        mdot_ox = rocket.mdot_ox(t)
         pressure_thrust = (Pa0 - Pa) * rocket.Ae
         thrust = np.array([rocket.thrust(t) + pressure_thrust, 0.0, 0.0])
         Isp = rocket.Isp + pressure_thrust / (mdot_p * g0)
-        # Limit Increase and Empty Oxidizers
-        if mdot_ox < 0.0:
-            mdot_ox = 0.0
-    # Limit Negative Propellant Mass
-    if mf <= rocket.mf_after:
-        mdot_f = 0.0
-        mf = rocket.mf_after
-    if mox <= 0.0:
-        mdot_ox = 0.0
-        mox = 0.0
+
     mp = mf + mox
     m = rocket.ms + mp
 
@@ -283,14 +295,14 @@ def dynamics(x, t, rocket, solver):
         solver.vel_launch_clear = Vel_Body[0]
         solver.acc_launch_clear = Acc_Body[0]
 
-    dx = np.zeros(18)
+    dx = np.zeros(16)
     dx[0:3] = Vel_ENU  # Pos_ENU
     dx[3:6] = Acc_ENU  # Vel_ENU
     dx[6:9] = Acc_Body  # Vel_Body
     dx[9:12] = omegadot  # omega_Body
     dx[12:16] = quatdot  # quat
-    dx[16] = -mdot_f  # mf
-    dx[17] = -mdot_ox  # mox
+    # dx[16] = -mdot_f  # mf
+    # dx[17] = -mdot_ox  # mox
 
     return dx
 
@@ -334,14 +346,14 @@ class Solver:
         quat0 = coord.euler2quat(rocket.azimuth0, rocket.elevation0, rocket.roll0)
         zero_array = np.array([[0.0] * 3])
 
-        x0 = np.zeros(18)
+        x0 = np.zeros(16)
         x0[0:3] = Pos0_ENU
         x0[3:6] = zero_array  # Vel_ENU
         x0[6:9] = zero_array  # Vel_Body
         x0[9:12] =  zero_array  # omega_Body
         x0[12:16] = quat0  # quat
-        x0[16] = rocket.m0_f  # mf
-        x0[17] = rocket.m0_ox  # mox
+        # x0[16] = rocket.m0_f  # mf
+        # x0[17] = rocket.m0_ox  # mox
         start_time = 0.0
         def estimate_end():
             It_digits = len(str(int(rocket.total_impulse)))
@@ -362,8 +374,10 @@ class Solver:
         self.Vel_Body_log = ode_log[:, 6:9]
         self.omega_log = ode_log[:, 9:12]
         self.quat_log = ode_log[:, 12:16]
-        self.mf_log = ode_log[:, 16]
-        self.mox_log = ode_log[:, 17]
+        # self.mf_log = ode_log[:, 16]
+        self.mf_log = rocket.mf(self.time_log)
+        # self.mox_log = ode_log[:, 17]
+        self.mox_log = rocket.mox(self.time_log)
         self.post_process(rocket)
 
     def post_process(self, rocket):
@@ -404,9 +418,12 @@ class Solver:
         elevation_log = attitude_log[:, 1]
         roll_log = attitude_log[:, 2]
         thrust_SL_log = rocket.thrust(time_log)
-        mdot_p_log = thrust_SL_log / (rocket.Isp * 9.80665)
-        mdot_f_log = rocket.mdot_f * np.array([thrust > 0.0 for thrust in rocket.thrust(time_log)])
-        mdot_ox_log = mdot_p_log - mdot_f_log
+        # mdot_p_log = thrust_SL_log / (rocket.Isp * 9.80665)
+        mdot_p_log = rocket.mdot_p(time_log)
+        # mdot_f_log = rocket.mdot_f * np.array([thrust > 0.0 for thrust in rocket.thrust(time_log)])
+        mdot_f_log = rocket.mdot_f(time_log)
+        # mdot_ox_log = mdot_p_log - mdot_f_log
+        mdot_ox_log = rocket.mdot_ox(time_log)
         pressure_thrust_log = (P_air_log[0] - P_air_log) * rocket.Ae
         thrust_log = np.array([thrust_SL + pressure_thrust if thrust_SL > 0.0 else 0.0 for thrust_SL, pressure_thrust in zip(thrust_SL_log, pressure_thrust_log)])
         Isp_log = rocket.Isp + np.array([pressure_thrust / (mdot_p * 9.80665) if mdot_p > 0.0 else 0.0 for pressure_thrust, mdot_p in zip(pressure_thrust_log, mdot_p_log)])