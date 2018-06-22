bug fix: Acc_ENU gliding of on-launcher dynamics, add: clash for pitch rotation analysis rocket to launcher

Simulator/dynamics.py
@@ -146,6 +146,8 @@ def onlauncher_dynamics(x, t, rocket, launch_site, quat0):
     DCM_ENU2Body = coord.DCM_ENU2Body_quat(quat0)
     DCM_Body2ENU = DCM_ENU2Body.transpose()
 
+    azimuth, elevation, roll = coord.quat2euler(DCM_ENU2Body)
+
     # alpha beta Vel_Air
     wind_ENU = env.Wind_ENU(launch_site.wind_speed(altitude), launch_site.wind_direction(altitude))
     Vel_air = DCM_ENU2Body.dot(Vel_ENU - wind_ENU)
@@ -173,12 +175,13 @@ def onlauncher_dynamics(x, t, rocket, launch_site, quat0):
     # Aero Force
     drag = dynamic_pressure * rocket.Cd(Mach) * rocket.A
     normal = dynamic_pressure * rocket.CNa(Mach) * rocket.A
-    F_aero = np.array([-drag, 0.0, 0.0])
+    F_aero = np.array([-drag, normal * beta, -normal * alpha])
 
     # Newton Equation
     Force = (thrust + F_aero)
-    Acc_Body =  Force / m + DCM_ENU2Body.dot(g)
-    Acc_ENU = DCM_Body2ENU.dot(Force) / m + g
+    # Acc_Body =  Force / m + DCM_ENU2Body.dot(g)
+    Acc_Body = np.array([Force[0] / m + g[2] / np.sin(np.deg2rad(elevation)), 0.0, 0.0])
+    Acc_ENU = DCM_Body2ENU.dot(Acc_Body)
 
     dx = np.zeros(12)
     dx[0:3] = Vel_ENU  # Pos_ENU
@@ -207,6 +210,8 @@ def onlauncher_tipoff_dynamics(x, t, rocket, launch_site, launcher_rail):
     # Translation coordinate
     DCM_ENU2Body = coord.DCM_ENU2Body_quat(quat)
     DCM_Body2ENU = DCM_ENU2Body.transpose()
+
+    azimuth, elevation, roll = coord.quat2euler(DCM_ENU2Body)
 
     # alpha beta Vel_Air
     wind_ENU = env.Wind_ENU(launch_site.wind_speed(altitude), launch_site.wind_direction(altitude))
@@ -244,11 +249,11 @@ def onlauncher_tipoff_dynamics(x, t, rocket, launch_site, launcher_rail):
     normal = dynamic_pressure * rocket.CNa(Mach) * rocket.A
     F_aero = np.array([-drag, normal * beta, -normal * alpha])
 
-
     # Newton Equation
     Force = (thrust + F_aero)
-    Acc_Body =  Force / m + DCM_ENU2Body.dot(g)
-    Acc_ENU = DCM_Body2ENU.dot(Force) / m + g
+    # Acc_Body =  Force / m + DCM_ENU2Body.dot(g)
+    Acc_Body = np.array([Force[0] / m + g[2] / np.sin(np.deg2rad(elevation)), 0.0, 0.0])
+    Acc_ENU = DCM_Body2ENU.dot(Acc_Body)
 
     # Center of Gravity
     Lcg_p = rocket.Lcg_p(t)    
@@ -268,7 +273,7 @@ def onlauncher_tipoff_dynamics(x, t, rocket, launch_site, launcher_rail):
             pivot_point = rocket.lower_lug  # ランチクリア後
         else:
             pivot_point = Lcg  # 上部ラグがランチクリア
-        
+
         # Inertia Moment
         Ij_pitch = rocket.Ij_pitch(t) + m * np.abs(Lcg - pivot_point) ** 2
         Ij_roll = rocket.Ij_roll(t)
@@ -280,7 +285,7 @@ def onlauncher_tipoff_dynamics(x, t, rocket, launch_site, launcher_rail):
 
         # Aero Moment
         moment_aero = np.array([0.0, F_aero[2] * (Lcp - pivot_point), -F_aero[1] * (Lcp - pivot_point)])
-
+        
         # Aero Dumping Moment
         moment_aero_dumping = np.zeros(3)
         # moment_aero_dumping[0] = dynamic_pressure * rocket.Clp * rocket.A * rocket.d ** 2 * 0.5 / Vel_air_abs * omega_Body[0]

extend_result_launcher.py
@@ -1,21 +1,20 @@
 # -*- coding: utf-8 -*-
-import os
-import sys
 import json
 import numpy as np
 import pandas as pd
-from scipy import interpolate
 import matplotlib.pyplot as plt
-
-from Simulator.rocket_param import Rocket
-
 
 # =====↓↓↓↓ USER INPUT ↓↓↓↓====
 config_file = 'rocket_config.json'
 result_dir = './Result_single_A'
 
 lug_clearance = 4.0  # [mm]
-lug_clearance /= 1e3  # [mm=>m]
+lug_clearance /= 1e3  # [mm => m]
+
+width_launcher = 70.0  # [mm]
+width_launcher /= 1e3  # [mm => m]
+fin_span = 150.0  # [mm]
+fin_span /= 1e3  # [mm => m]
 # =====↑↑↑↑ USER INPUT ↑↑↑↑====
 
 # config file to json
@@ -31,10 +30,6 @@ launcher_rail = launch_pad.get('Launcher Rail Length [m]')
 # read log
 df = pd.read_csv(result_dir+'/log.csv', index_col=False)
 
-
-time_launch_clear = float(open(result_dir+'/result.txt').readline().split(',')[1].split('[s]')[0])
-index_launch_clear = np.argmax(df['time'] >= time_launch_clear)
-
 # Pitch
 elv0 = df['elevation'][0]
 launcher_downrange = np.array([0.0, launcher_rail * np.cos(np.deg2rad(-elv0))])
@@ -44,28 +39,28 @@ Pos_Lcg_altitude = df['Pos_Up'] - (R + lug_clearance) * np.cos(np.deg2rad(-df['e
 Pos_End_downrange = Pos_Lcg_downrange - (L - df['Lcg']) * np.cos(np.deg2rad(-df['elevation'])) - (R) * np.sin(np.deg2rad(-df['elevation']))
 Pos_End_altitude = Pos_Lcg_altitude - (L - df['Lcg']) * np.sin(np.deg2rad(-df['elevation'])) + (R) * np.cos(np.deg2rad(-df['elevation']))
 
+distance_End = np.sqrt(Pos_End_altitude ** 2 + Pos_End_downrange ** 2)
+index_rocket_clear = np.array(distance_End > np.sqrt(launcher_altitude[-1] ** 2 + launcher_downrange[-1] ** 2)).argmax() + 1
 
 plt.figure()
 plt.plot(launcher_downrange, launcher_altitude, label='Launcher', color='black')
-plt.plot(Pos_End_downrange, Pos_End_altitude, label='Trajectory - End')
-plt.plot(Pos_Lcg_downrange, Pos_Lcg_altitude, label='Trajectory - Lcg')
+plt.plot(Pos_End_downrange[:index_rocket_clear], Pos_End_altitude[:index_rocket_clear], label='Trajectory - End')
+plt.plot(Pos_Lcg_downrange[:index_rocket_clear], Pos_Lcg_altitude[:index_rocket_clear], label='Trajectory - Lcg')
+plt.xlabel('Downrange [m]')
+plt.ylabel('Altitude [m]')
 plt.legend()
 plt.grid()
-# plt.show()
-
-
+plt.show()
 
 distance = lug_clearance - ((L - df['Lcg']) * np.tan(np.deg2rad(df['elevation'] - elv0)))
 
 plt.figure()
-plt.plot(df['time'], distance * 1e3)
-# plt.xlim(xmax=time_launch_clear)
-
-
-
-plt.show()
+plt.plot(df['time'][:index_rocket_clear], distance[:index_rocket_clear] * 1e3)
+plt.xlabel('Time [sec]')
+plt.ylabel('Distance Launcher - Rocket end [mm]')
+plt.axhline(y=0.0, color='black')
+plt.xlim(xmin=0.0, xmax=df['time'][index_rocket_clear-1])
+plt.grid()
+plt.savefig(result_dir + '/Distance_launcher_rocket.png')
 
 # Yaw
-
-# azimuth,elevation Lcg
-