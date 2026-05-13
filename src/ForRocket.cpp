30464d02b59ea94e118c1df8f289585e14ee41f0
sus304 <sus304.tanaka@gmail.com>
2026-05-13 19:19:28 +0900

    update ver.4.2.2

--- src/ForRocket.cpp
+++ src/ForRocket.cpp
@@ -14,8 +14,8 @@
 #include "commandline_option.hpp"
 #include "solver/trajectory_solver.hpp"
 
-const std::string program_ver = "4.2.1";
-const std::string release_date = "2026 May 6";
+const std::string program_ver = "4.2.2";
+const std::string release_date = "2026 May 13";
 
 int main(int argc, char* argv[]) {
     // arg parse
